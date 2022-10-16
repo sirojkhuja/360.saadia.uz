3924cc61f085cb7434c4a5ad646f54a39a27713c
Sirojkhuja Muminkhodjaev <sirojxuja@gmail.com>
2022-10-16 10:51:56 +0000

    Some changes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express =  require('express');
 const app = express();
 const path = require('path');
+const PORT = 36036;
+
 
 app.use(express.static(__dirname + '/public'));
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
-app.listen(3000, ()=>{
-    console.log('visit http://localhost:3000/');
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port: ${PORT}`);
 });
